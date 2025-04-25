d0dd35cefcb34938edda9d5748ddd63f5787f865
Lanlinju <lanlinju@outlook.com>
2025-04-25 13:08:07 +0800

    build(simple-gradle): update main class and add jpackage task
    - Update main class in manifest to com.example.MainKt
    - Add jpackage task for creating native packages on Windows
    - Set up task dependencies and configuration for jpackage

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,10 +21,31 @@ application {
 
 tasks.jar {
     manifest {
-        attributes["Main-Class"] = "com.example.HelloKt"
+        attributes["Main-Class"] = "com.example.MainKt"
         attributes["Implementation-Version"] = project.version
     }
 }
+
+
+/* The Windows platform requires the installation of `WiX Toolset` for exe and msi. */
+tasks.register<Exec>("jpackage") {
+    group = "distribution"
+    dependsOn("installDist")
+    val type = "app-image" // ✅ 或者 exe（仅 Windows）
+    commandLine(
+        "jpackage",
+        "--name", "MyApp",
+        "--input", "build/install/simple-gradle/lib",
+        "--main-jar", "simple-gradle-${version}.jar",
+        "--main-class", "com.example.HelloDialogKt",
+        "--type", type, // 或 "dmg", "pkg", "deb", "rpm", app-image等
+        "--dest", "build/dist",
+        "--app-version", version,
+        "--vendor", "company",
+    )
+}
+
+// ./gradlew jpackage
 
 tasks.test {
     useJUnitPlatform()
